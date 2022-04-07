1a3f4c1020fe3809bcf5b4e3441cc9ba78140aba
David Wood <david.wood@saabgroup.com>
2022-04-07 12:04:51 +0100

    Initial commit

---
+++ Saab.Time.Abstractions/Delegates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Saab.Time
+{
+    public delegate DateTime UtcNow();
+
+    public delegate CancellationTokenSource CreateCancellationTokenSource(TimeSpan timeout);
+
+    public delegate Task TaskDelay(TimeSpan timeout, CancellationToken ct = default);
+
+    public delegate ITimer CreateTimer();
+}
---
+++ Saab.Time.Abstractions/ITimer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Timers;
+
+namespace Saab.Time
+{
+    public interface ITimer : IDisposable
+    {
+        bool AutoReset { get; set; }
+
+        bool Enabled { get; set; }
+
+        /// <summary>
+        ///     Number of milliseconds between <see cref="Elapsed"/> events being triggered.
+        /// </summary>
+        double Interval { get; set; }
+
+        void Start();
+
+        void Stop();
+
+        event ElapsedEventHandler Elapsed;
+    }
+}
